41beba779ff6c22c0580050ade00420337ae9896
Alejandro Barrios <ottoabarriosp@hotmail.com>
2022-12-30 22:25:54 -0400

    feat: create initial route handler

--- src/handler.js
+++ src/handler.js
@@ -1,5 +1,27 @@
+import { parse } from "node:url";
+import { DEFAULT_HEADER } from "./utils/util";
+
+const allRoutes = {
+  "/heroes:get": (request, response) => {
+    response.write("GET");
+    response.end();
+  },
+  // Fallback
+  default: (request, response) => {
+    response.writeHead(404, DEFAULT_HEADER);
+    response.write("not found");
+    response.end();
+  },
+};
+
 function handler(request, response) {
-  response.end("Hello world!");
+  const { url, method } = request;
+  const { pathname } = parse(url, true);
+
+  const key = `${pathname}:${method.toLowerCase()}`;
+  const urlHandler = allRoutes[key] || allRoutes.default;
+
+  return urlHandler(request, response);
 }
 
 export default handler;
