19b1709f621325b1f782a404289c0631e9a70811
Alejandro Barrios <ottoabarriosp@hotmail.com>
2022-12-30 23:30:15 -0400

    test: create integration test

---
+++ tests/integration/hero.test.js
@@ -0,0 +1,49 @@
+import test from "node:test";
+import assert from "node:assert";
+import { promisify } from "node:util";
+
+// each callback passed to "test" receives the context (t)
+test("Hero Integration Test Suite", async (t) => {
+  const testPort = 9009;
+  process.env.PORT = testPort;
+
+  const { server } = await import("../../src/index.js");
+  const testServerAddress = `http://localhost:${testPort}/heroes`;
+
+  await t.test("it sould create a hero", async (t) => {
+    const data = {
+      name: "Batman",
+      age: 50,
+      power: "rich",
+    };
+
+    const request = await fetch(testServerAddress, {
+      method: "POST",
+      body: JSON.stringify(data),
+    });
+
+    assert.deepStrictEqual(
+      request.headers.get("content-type"),
+      "application/json",
+      "it should return a valid content-type header"
+    );
+
+    assert.strictEqual(
+      request.status,
+      201,
+      'it should return "created" status code'
+    );
+
+    const result = await request.json();
+    assert.deepStrictEqual(
+      result.success,
+      "User created with success!!",
+      "it should return a valid response"
+    );
+    assert.ok(result.id.length > 30, "it should be a valid uuid");
+  });
+
+  // We use 'bind' to ensure that internal 'this' context for
+  // server.close method won't be missed
+  await promisify(server.close.bind(server))();
+});
